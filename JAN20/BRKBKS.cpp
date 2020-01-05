5a361d9b7c36bbf771743157c662fa9c1e2427d8
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-01-05 12:11:58 +0530

    long challenge jan 2020

---
+++ JAN20/BRKBKS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	int t,i,j,w1,w2,w3,s;
+	cin>>t;
+	while(t--)
+	{
+		cin>>s>>w1>>w2>>w3;
+		
+		if(w1+w2+w3 <=s)
+			cout<<1<<endl;
+		else if(w1+w2 <= s || w2+w3 <= s)
+			cout<<2<<endl;
+		else
+			cout<<3<<endl;		
+	}
+	return 0;
+}
---
+++ JAN20/DYNAMO.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		int n;
+		scanf("%d",&n);
+		long long int a,s,b,c,d,e;
+		scanf("%lld",&a);
+		s = 50;
+		printf("%lld",s);
+		fflush(stdin);
+		scanf("%lld",&b);
+		c= b;
+		printf("%lld",c);
+		fflush(stdin);
+		scanf("%lld",&d);
+		e = d;
+		printf("%lld",e);
+		fflush(stdin);
+		int re;
+		scanf("%d",&re);
+		if(re == -1)
+			continue;
+	
+	}
+	return 0;
+}
