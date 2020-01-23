5174c3b5bd8d3b395d996609bd48814e52f0345e
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-01-23 14:48:04 +0530

    Cake walk

--- COOK114/CHFCHK.c
+++ COOK114/CHFCHK.c
@@ -1,1 +1,27 @@
-R
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int N;
+	cin>>N;
+	int i;
+	long int a[N],smallest;
+
+	for(i = 0;i<N;i++)
+	{
+		cin>>a[i];
+		if(i==0)
+			smallest =a[i];
+		else
+			if(smallest > a[i])
+				smallest = a[i];
+	}
+	cout<<smallest<<endl;
+	}
+	return 0;
+}
