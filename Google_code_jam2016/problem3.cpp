e1e8fa8a6bdb074be1b70141e6730cd1023ffc88
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-02-01 12:15:37 +0530

    problem in power function

--- Google_code_jam2016/problem3.cpp
+++ Google_code_jam2016/problem3.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-long long int fact(int n)
+long long int M = 1000000007;
+long long int nCr(int n,int r)
 {
-    long long int res = 1;
-    for (int i = 2; i <= n; i++)
-        res = res * i;
-    return res;
-} 
+	long long int temp=1;
+	long long int d = 1;
+	while(r>0)
+	{
+	
+		temp = (temp*n)%M;
+		d = d*r;
+		if(temp%d == 0)
+		{
+			temp = temp/d;
+			d = 1;
+		
+		}
+		n--;
+		r--;
+	
+	}
+	
+	return temp%M;
+	
 
-long long int nCr(int n, int r) 
-{ 
-    return fact(n) / (fact(r) * fact(n - r)); 
 } 
-      
 int main()
-{	long long int M = 1000000007;
+{	
+	long long int r,f_t,temp,total,s_t,t_t;
 	int T;
 	cin>>T;
 	int j = 1;
@@ -23,16 +36,19 @@ int main()
 	{
 	int c,v,l;
 	cin>>c>>v>>l;
-	long long int total = 0;
+    total = 0;
 	int i=0;
 	cout<<"Case #"<<j<<": "; 
 	while(1)
 	{
-		int s_t = c*v;
-		long long int f_t = nCr(l-i,i);
-		long long int temp = (  (f_t) * pow(s_t,i) * pow(v,l-(2*i)) );
+		 s_t = c*v;
+		 f_t = nCr(l-i,i);
+		 s_t =  pow(s_t,i);
+		 t_t = pow(v,l-(2*i)) ;
+		temp = (  f_t * s_t%M * t_t%M)%M;
+		cout<<f_t<<" "<<s_t<<" "<<" "<<t_t<<" "<<temp<<endl;
 		total = total + temp;
-//		cout<<i<<"::"<<temp<<endl;
+		//cout<<i<<"::"<<temp<<endl;
 		i++;
 		if(l-2*i<0)
                         break;
