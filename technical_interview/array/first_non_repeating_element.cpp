826980398ab2f5f14c5dad5e538961fba5e534d0
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-03-21 15:11:36 +0530

    first_non_repeating_element in an array o(n) solution

---
+++ technical_interview/array/first_non_repeating_element.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+	
+	 unordered_map<int, int> mp;
+
+	 int n;
+	 cin>>n;
+	 int a[n];
+	 for(int i=0;i<n;i++)
+	 	cin>>a[i];
+	
+	 for(int i=0;i<n;i++)
+	 	mp[a[i]]++;
+	 for(int i =0;i<n;i++)
+	 {
+		if(mp[a[i]] == 1)
+		{
+			cout<<a[i]<<endl;
+			break;
+		}
+	 }
+	 
+	return 0;
+}
+
+/*How to compile this code --> g++ -std=c++0x first_non_repeating_element.cpp 
+time complexity --O(n)
+*/
