7bc6769124af745d995ce43ca5b2573b0bd49fde
Drisya POnmari <111701031@smail.iitpkd.ac.in>
2020-03-21 11:17:39 +0530

    finding the second minimum element in an array

---
+++ technical_interview/array/second_min.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i =0 ; i<n ; i++)
+		cin>>a[i];
+	
+	int first = INT_MAX,second = INT_MAX;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i] <= first)
+		{
+			second = first;
+			first = a[i];
+		}
+		else if(a[i] <= second)
+			second = a[i];
+	}
+
+	cout<<second<<endl;
+	
+	return 0;
+}
